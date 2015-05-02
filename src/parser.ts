9f86c20e5beca62975e06156fa35fbe5aad42c61
Kitson Kelly <dojo@kitsonkelly.com>
2015-05-02 22:11:33 +0100

    Add basic functionality to parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,18 +1,33 @@
+import core = require('dojo/interfaces');
+import Registry = require('dojo/Registry');
+import WeakMap = require('./WeakMap');
 
-interface ParserDefinitionOptions {
-    object?: Object;
+'use strict';
+
+export interface ParserObject {
+    node: HTMLElement;
+    id: string;
+}
+export interface ParserObjectConstructor {
+    new (node?: HTMLElement): ParserObject;
+    prototype: ParserObject;
+}
+declare var ParserObject: ParserObjectConstructor;
+
+interface IParserDefinitionOptions {
+    proto?: {};
+    Ctor?: Function;
     doc?: Document;
     extensionOf?: string;
 }
 
-interface RemovalHandle {
-    remove(): void;
+interface IRegistrationHandle extends core.IHandle {
+    Ctor: ParserObjectConstructor;
 }
 
-interface DefinitionHandle {
-    remove(): void;
-    ctor: Function;
-}
+var parserRegistry = new Registry(null);
+var parserIDMap: { [id: string]: ParserObject } = {};
+var parserNodeMap: WeakMap<any, any> = new WeakMap();
 
 var _hasMutationObservers: boolean;
 
@@ -33,14 +48,19 @@ function observervationCallback(observations: MutationRecord[]): void {
         if (observation.type === 'childList') {
             addedNodes = slice.call(observation.addedNodes);
             addedNodes.forEach((node: HTMLElement) => {
-                if (node.nodeType === 1) {
-                    console.log('add', node.tagName.toLowerCase(), node.getAttribute('as'));
+                var Ctor: ParserObjectConstructor = parserRegistry.match(node);
+                var obj: ParserObject = new Ctor(node);
+                obj.node = node;
+                if (node.id) {
+                    obj.id = node.id;
+                    parserIDMap[obj.id] = obj;
                 }
+                parserNodeMap.set(node, obj);
             });
             removedNodes = slice.call(observation.removedNodes);
             removedNodes.forEach((node: HTMLElement) => {
                 if (node.nodeType === 1) {
-                    console.log('remove', node.tagName.toLowerCase(), node.getAttribute('as'));
+                    console.log('remove', node.tagName.toLowerCase(), node.getAttribute('is'));
                 }
             });
         }
@@ -48,7 +68,7 @@ function observervationCallback(observations: MutationRecord[]): void {
     });
 }
 
-export function watch(doc: Document = document): RemovalHandle {
+export function watch(doc: Document = document): core.IHandle {
     if (hasMutationObservers() && typeof observer === 'undefined') {
         observer = new MutationObserver(observervationCallback);
         observer.observe(doc.body, {
@@ -60,18 +80,51 @@ export function watch(doc: Document = document): RemovalHandle {
         remove(): void {
             if (observer) {
                 observer.disconnect();
+                observer = undefined;
             }
         }
     };
 }
 
-export function define(tagName: string, options: ParserDefinitionOptions): DefinitionHandle {
+export function register(tagName: string, options: IParserDefinitionOptions): IRegistrationHandle {
+    tagName = tagName && tagName.toLowerCase();
+    var Ctor: Function;
+    if (!options.Ctor && options.proto) {
+        Ctor = function ParserObject() { };
+        Ctor.prototype = <ParserObject> options.proto;
+    }
+    else if (options.Ctor) {
+        Ctor = options.Ctor;
+    }
+    else {
+        throw new SyntaxError('Missing either "Ctor" or "proto" in options.');
+    }
+    var handle = parserRegistry.register((node: HTMLElement) => {
+        if (node.tagName.toLowerCase() === tagName) {
+            return true;
+        }
+        var attrIs: string = node.getAttribute('is');
+        if (attrIs && attrIs.toLowerCase() === tagName) {
+            return true;
+        }
+    }, Ctor);
+
     return {
-        remove(): void { },
-        ctor(): void { }
+        remove(): void {
+            handle.remove();
+        },
+        Ctor: <ParserObjectConstructor> Ctor
     };
 }
 
 export function scan(doc: Document = document): any[] {
     return [];
+}
+
+export function byId(id: string) {
+    return parserIDMap[id];
+}
+
+export function byNode(node: HTMLElement) {
+    return parserNodeMap.get(node);
 }
--- tests/unit/parser.ts
+++ tests/unit/parser.ts
@@ -2,14 +2,45 @@ import assert = require('intern/chai!assert');
 import registerSuite = require('intern!object');
 import parser = require('src/parser');
 
+interface ParserTestInterface extends parser.ParserObject {
+    foo: any;
+}
+
 registerSuite({
     name: 'parser',
     'basic': function () {
+        if (typeof document === 'undefined') {
+            this.skip('No DOM');
+        }
         var dfd = this.async();
-        var handle = parser.watch();
-        document.body.innerHTML = '<div as="test-div"></div>';
+        var watchHandle = parser.watch();
+
+        var TestCtor = function TestCtor(node: HTMLElement) {
+            assert(node);
+        };
+        TestCtor.prototype = <ParserTestInterface> {
+            'foo': 'bar'
+        };
+
+        var registerHandle = parser.register('test-div', {
+            Ctor: TestCtor
+        });
+
+        document.body.innerHTML = '<div is="test-div" id="test1"></div><test-div id="test2"></test-div>';
+
+        var test1 = document.getElementById('test1');
+        var test2 = document.getElementById('test2');
         setTimeout(function () {
-            handle.remove();
+            assert.equal((<ParserTestInterface> parser.byId('test1')).foo, 'bar');
+            assert.strictEqual(parser.byId('test1').node, test1);
+            assert.strictEqual(parser.byNode(test1).id, 'test1');
+
+            assert.equal((<ParserTestInterface> parser.byId('test2')).foo, 'bar');
+            assert.strictEqual(parser.byId('test2').node, test2);
+            assert.strictEqual(parser.byNode(test2).id, 'test2');
+
+            watchHandle.remove();
+            registerHandle.remove();
             dfd.resolve();
         }, 1);
     }
